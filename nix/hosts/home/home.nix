d2a887cb31a38cae0203af89a5f0f66bc2c4bee8
Wojciech Kozyra <wkozyra95@gmail.com>
2026-04-27 21:26:09 +0200

    enforce max cargo build jobs

--- nix/hosts/home/home.nix
+++ nix/hosts/home/home.nix
@@ -25,6 +25,10 @@ systemModules:
       git.signingKey = "35DF 8DFA D0E7 1E39 F047 BD01 AE51 A568 2B78 648C";
     };
 
+    home.sessionVariables = {
+      CARGO_BUILD_JOBS = "16";
+    };
+
     programs.gpg.enable = true;
     services.gpg-agent = {
       enable = true;
